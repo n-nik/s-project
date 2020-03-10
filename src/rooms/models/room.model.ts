77dfdaf7ebcbb5aad985ac116155b381f3c53bd4
n-nik <n.nagoryansky@gmail.com>
2020-03-10 12:32:48 +0200

    Added bookedRoomCount, isAvailable fields to rooms list api. Added filter by dates. Some improvements

--- src/rooms/models/room.model.ts
+++ src/rooms/models/room.model.ts
@@ -3,9 +3,30 @@ import { DataType } from 'sequelize-typescript';
 import { Location } from '../../locations/models/location.model';
 import { RoomType } from '../interfaces/room.interface';
 import { CommonScopes } from '../../common/models/commonScopes';
+import { literal } from 'sequelize';
 
 @Scopes(() => ({
   pagination: CommonScopes.getPaginationScope(),
+
+  filterByLocation: (locationId: number) => ({
+    where: { locationId }
+  }),
+
+  withBookedCount: (startDate: string, endDate: string) => {
+    return {
+      attributes: {
+        include: [
+          [literal(`
+            (SELECT COUNT(*) FROM \`RoomBookings\` as rb
+              WHERE 
+              rb.roomId = \`Room\`.\`id\` 
+              AND (rb.startDate BETWEEN '${startDate}' AND '${endDate}')
+              AND (rb.endDate BETWEEN '${startDate}' AND '${endDate}'))
+          `.replace(/\s+/g, ' ')), 'bookedRoomCount']
+        ]
+      }
+    }
+  }
 }))
 @Table
 export class Room extends Model<Room> {
